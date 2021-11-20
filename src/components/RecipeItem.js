9665dfb917d96509dba16c118228185ab13207d0
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-20 16:40:48 -0500

    made a rough recipe list  item

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function RecipeItem(props) {
+
+  
+  return (
+    <div>
+     <li className="text--regular"> recipe.name</li> 
+     <li className="text--light"> category.name </li>
+     <li className="text--light"> recipe.description </li> 
+     <li className="text--light"> recipe.Ingredients </li>
+     <li className="text--light"> recipe.steps</li> 
+     <li className="text--light"> recipe.servings</li> 
+     <li className="text--light"> recipe.time </li>
+    </div>
+
+  );
+}
