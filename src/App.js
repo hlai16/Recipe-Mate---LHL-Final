1dc50b69832f427e17884174e32b09733bec0f00
hlai16 <gloriajoanlai@gmail.com>
2021-11-21 18:16:34 -0500

    editting nav route logic for login function

--- src/App.js
+++ src/App.js
@@ -50,11 +50,11 @@ function App() {
       <BrowserRouter>
         <section className="navDiv">
           <Navbar bg="white" expand="lg" sticky="top">
-            <Navbar.Brand><Link to={'/'} className="nav-link"><img src="https://github.com/hlai16/Recipe-Mate---LHL-Final/blob/feature/search-index/public/images/logo.png?raw=true" alt="logo" width="100"></img></Link></Navbar.Brand>
+            <Navbar.Brand><Link to={'/user_profile'} className="nav-link"><img src="https://github.com/hlai16/Recipe-Mate---LHL-Final/blob/feature/search-index/public/images/logo.png?raw=true" alt="logo" width="100"></img></Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link><Link to={'/'} className="nav-link">Home</Link></Nav.Link>
+                <Nav.Link><Link to={'/user_profile'} className="nav-link">Home</Link></Nav.Link>
                 <Nav.Link><Link to={'/search'} className="nav-link">Categories</Link></Nav.Link>
                 <Nav.Link><Link to={'/create'} className="nav-link">Create</Link></Nav.Link>
 
