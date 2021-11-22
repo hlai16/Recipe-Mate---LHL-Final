6539353a873ab15dbe40474be35270d89e5e1e40
hlai16 <gloriajoanlai@gmail.com>
2021-11-22 16:21:36 -0500

    nope, the PropTypes ain't working

--- src/components/Login.js
+++ src/components/Login.js
@@ -51,7 +51,7 @@ function Login(props, { setToken }) {
           email,
           password
         });
-        setToken(token);
+        props.setToken(token);
       }
     
     return (
@@ -84,7 +84,7 @@ function Login(props, { setToken }) {
     );
 }
 
-Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-}
+// Login.propTypes = {
+//     setToken: PropTypes.func.isRequired
+// }
 export default Login;
