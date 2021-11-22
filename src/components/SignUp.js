7bbcc90cd79bbac85e5ecd6da764815e51663a18
hlai16 <gloriajoanlai@gmail.com>
2021-11-22 00:27:00 -0500

    refactoring signup logic

---
+++ src/components/SignUp.js
@@ -0,0 +1,63 @@
+
+import Buttons from './Buttons.js';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import UserProfile from './AuthPages/UserProfile';
+import { Form } from "react-bootstrap";
+
+function Signup(props) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    function validate() {
+        if (email === "") {
+            setError("Email cannot be blank");
+            return;
+        }
+        if (password === "") {
+            setError('Please enter your password');
+            return;
+        }
+        setError('');
+        props.onSave(email, password);
+        // form proceed to save if student != "" || interviewer is selected
+    }
+    const reset = function () {
+        return setEmail(''), setPassword('');
+    }
+    const cancel = function () {
+        return props.onCancel(reset());
+    }
+    return (
+        <Form method="GET" action="/user_profile" onSubmit={props.onSubmit}>
+            <h4>Please enter your signup info:</h4>
+            {/* <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>User Name</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" username={username} onChange={(event) => setUsername(event.target.value)} />
+            </Form.Group> */}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" email={email} onChange={(event) => setEmail(event.target.value)} required />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" password={password} onChange={(event) => setPassword(event.target.value)} required />
+            </Form.Group>
+
+            <Buttons type="submit" onClick={validate}>
+                Submit
+            </Buttons>
+            <Buttons onClick={cancel}>
+                Back
+            </Buttons>
+        </Form>
+    );
+}
+
+export default Signup;
