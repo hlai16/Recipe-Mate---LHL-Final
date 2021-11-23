922d74579b4147fb1175d0626501ec4bd88ac375
hlai16 <gloriajoanlai@gmail.com>
2021-11-22 19:14:21 -0500

    adding navbar to Create

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
   const { state } = useApplicationData();
   const { token, setToken } = useToken();
  
-  if (!token) {
-    return <Home setToken={setToken} />
-   } 
+  // if (!token) {
+  //   return <Home setToken={setToken} />
+  //  } 
   //  else {
   //   return (
   //     <Login
