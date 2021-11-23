31c1a4cc23d166065c8880ee6ab957ab143ef03c
Ana Carolina K <anacko@gmail.com>
2021-11-23 15:18:09 -0500

    add recipes route for getting the search results. not connected yet.

--- src/components/Helpers/getRecipesWithSearch.js
+++ src/components/Helpers/getRecipesWithSearch.js
@@ -1,28 +1,17 @@
 import axios from "axios";
 
-
 export default function getRecipesWithSearch(event) {
   event.preventDefault()
 
   const search = event.target[0].value
-  axios.get(`/search_recipes?search=${search}`)
-  .then((all) => {
-      const recipes = all.data
+  axios
+    .get(`/search_recipes?search=${search}`)
+    .then((res) => {
+      const recipes = res.data
       console.log("RECIPES:", recipes)
-    });
-
-      // for (const user of Users) {
-      //   if (user.email === email) {
-      //     if (user.password === password) {
-      //       console.log("User id is:", user.id)
-      //       return user.id
-      //     } 
-        //   else {
-        //     return "Incorrect password"
-        //   }
-        // } else {
-        //   return "Email not found"
-        //}
-      //}
-  //return userId
+    })
+    
+    // Should return a redirect to the recipe list with props being the array of recipes.
+    // If it's not the case, RecipeList component will need changes
+    // Also check App.js route for /recipe
 }
