a3b608ba385949225f5da2caa7ceb8e4fa4319b8
Ana Carolina K <anacko@gmail.com>
2021-11-24 16:04:30 -0500

    add functionality to connect recipes to single recipe view

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,20 +1,26 @@
 import { useLocation } from "react-router";
+import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 
 export default function RecipeList(props) {
   
   const location = useLocation();
   const recipes = location.state || []
+
+  let navigate = useNavigate()
+  const handleClick = function(event) {
+    navigate(`/SingleRecipe`, { state: event.target.id })
+  }
   
   return (< > 
     <NavBar setToken={props.setToken} />
     <h2>Search Results</h2>
     {recipes.map((recipe, key) => (
-      <article key={key}>
-        <h3>{recipe.name}</h3>
-        <img src={recipe.image} alt="display image" height="70" width="100"/>
-        <p>Category: {recipe.category_name}</p>
-        <p>Likes: {recipe.likes}</p>
+      <article key={key} onClick={handleClick} id={recipe.id}>
+        <h3 id={recipe.id}>{recipe.name}</h3>
+        <img id={recipe.id} src={recipe.image} alt="display image" height="70" width="100"/>
+        <p id={recipe.id}>Category: {recipe.category_name}</p>
+        <p id={recipe.id}>Likes: {recipe.likes}</p>
       </article>
     ))}
 
