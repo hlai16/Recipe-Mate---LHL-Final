d909ed8f99cc90fb52af3eaa1cd4a660cf3364a8
hlai16 <gloriajoanlai@gmail.com>
2021-11-24 15:56:28 -0500

    some css in RecipeById

--- src/components/SingleRecipe/RecipeById.js
+++ src/components/SingleRecipe/RecipeById.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './RecipeById.scss';
+import { render } from "react-dom";
+
+// get our fontawesome imports
+import { faHome, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function RecipeById(props) {
   const [recipeById, setRecipeById] = useState('');
@@ -12,21 +17,24 @@ export default function RecipeById(props) {
     axios
       .get(`/Recipes/${recipeId}`)
       .then((results) => setRecipeById(results.data[0]))
-    }, []);
-    
-    useEffect(() => {
-      if (recipeById.user_id) {
-        axios.get(`/Users/${recipeById.user_id}/recipes`)
+  }, []);
+
+  useEffect(() => {
+    if (recipeById.user_id) {
+      axios.get(`/Users/${recipeById.user_id}/recipes`)
         .then((results) => (
           setMoreFromUser(results.data)
-          ))
-      }
-    }, [recipeById])
+        ))
+    }
+  }, [recipeById])
 
   return (< >
     <h3>{recipeById.name}</h3>
-    <p>Likes: {recipeById.likes}</p>
-    <img src={recipeById.image_link} alt="display image" height="300" width="400"/>
+    <div className="likesDiv">
+      <span className="likes"><FontAwesomeIcon icon={faThumbsUp} />{recipeById.likes}</span>
+    </div>
+
+    <img src={recipeById.image_link} alt="display image" height="300" width="400" />
     <h4>About</h4>
     <p>{recipeById.description}</p>
     <table className="table recipe-presentation">
@@ -61,10 +69,10 @@ export default function RecipeById(props) {
         <div>
           <div>Recipe ID: {recipe.id}</div>
           <div>Name: {recipe.name}</div>
-          <div>Image: <img src={recipe.image_link} alt="display image" height="50" width="80"/></div>
+          <div>Image: <img src={recipe.image_link} alt="display image" height="50" width="80" /></div>
         </div>))}
       </div>
-    
+
     </table>
 
   </>);
