c384ce077e23f96dc8894526cfd9366ccfdd7f94
Ana Carolina K <anacko@gmail.com>
2021-11-23 20:08:46 -0500

    connect search recipes to the recipes route

---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { Form, FormControl, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import Buttons from './Buttons';
+
+function SearchRecipes() {
+  let navigate = useNavigate()
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await axios
+      .get(`/search_recipes?search=${event.target[0].value}`)
+      .then((res) => {
+        navigate("/recipes", { state: res.data })
+      }).then(() => {
+      })
+  }
+
+  return (
+    <Form inline onSubmit={handleSubmit}>
+      <Row>
+        <Col><FormControl type="text" placeholder="Search" className="mr-sm-2" /></Col>
+        <Col><Buttons small>Search</Buttons></Col>
+      </Row>
+    </Form>)
+}
+
+export default SearchRecipes
