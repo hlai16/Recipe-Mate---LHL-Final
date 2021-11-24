4dfdb42610a6dce8d19c839e5f5a895cf11423f8
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-24 14:56:48 -0500

    remove old auth code

--- src/components/AuthPages/index.js
+++ src/components/AuthPages/index.js
@@ -1,19 +1,9 @@
+import UserProfile from './UserProfile';
+import NavBar from '../NavBar';
+// import Home from '../Home';
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 // import Search from './components/Search/index';
 // import Create from './components/Create';
-import UserProfile from './UserProfile';
-import NavBar from '../NavBar';
-import Home from '../Home';
-
-
-
-// const RequiresLogin = (props) => {
-//   const isLoggedIn = true;
-//   if(isLoggedIn) { 
-//     return props.children;
-//   } 
-//   return <Home />
-// }
 
 
 function AuthPages(props) {
