509d81cc6fa350ee194241fae533af1afca6e212
hlai16 <gloriajoanlai@gmail.com>
2021-11-26 01:58:28 -0500

    Favorite card styling done

--- src/components/AuthPages/FavoriteItem.js
+++ src/components/AuthPages/FavoriteItem.js
@@ -1,6 +1,9 @@
 import React from "react";
 import classNames from "classnames";
 import './FavoriteItem.scss';
+import Buttons from '../Buttons';
+import { faBookReader, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FavoriteItem(props) {
     // to toggle selected class
@@ -11,6 +14,14 @@ export default function FavoriteItem(props) {
             <div className="recipeCard--text">
                 <h5>{props.name}</h5>
                 <div>{props.description}</div>
+            </div>
+            <div className="recipeCard--buttons">
+                <div>
+                    <h1 className="view"><FontAwesomeIcon icon={faBookReader} /></h1>
+                </div>
+                <div>
+                    <h1 className="delete"><FontAwesomeIcon icon={faTrashAlt} /></h1>
+                </div>
             </div>
         </div>
     );
