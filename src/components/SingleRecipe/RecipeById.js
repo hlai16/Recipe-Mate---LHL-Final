85aa132c836da4794075871d97f09ebef84a334a
Ana Carolina K <anacko@gmail.com>
2021-11-27 19:19:56 -0500

    add update likes

--- src/components/SingleRecipe/RecipeById.js
+++ src/components/SingleRecipe/RecipeById.js
@@ -16,6 +16,7 @@ import { array } from "prop-types";
 export default function RecipeById(props) {
   const [recipeById, setRecipeById] = useState('');
   const [moreFromUser, setMoreFromUser] = useState([]);
+  const [hasLike, setHasLike] = useState(false);
   const [message, setMessage] = useState('');
 
 
@@ -31,7 +32,6 @@ export default function RecipeById(props) {
         console.log('results.data[0] after setRecipeById', results.data[0])
         console.log('results.data[0].id after setRecipeById', results.data[0].id)
       })
-
   }, [recipeId]);
 
   useEffect(() => {
@@ -43,6 +43,23 @@ export default function RecipeById(props) {
     }
   }, [recipeById])
 
+  const handleLikes = () => {
+    let prevLikesAmount = recipeById.likes;
+    console.log("Likes before: ", prevLikesAmount)
+    if (hasLike) {
+      const newRecipeState = {...recipeById, likes: prevLikesAmount - 1}
+      axios
+        .put(`/Recipes/${recipeById.id}/likes`, {likes: prevLikesAmount - 1})
+        .then(setRecipeById(newRecipeState))
+        .then(setHasLike(false))
+    } else {
+      const newRecipeState = {...recipeById, likes: prevLikesAmount + 1}
+      axios
+        .put(`/Recipes/${recipeById.id}/likes`, {likes: prevLikesAmount + 1})
+        .then(setRecipeById(newRecipeState))
+        .then(setHasLike(true))
+    }
+  }
 
 
   console.log(recipeById)
@@ -78,7 +95,7 @@ export default function RecipeById(props) {
   return (< >
     <h3>{recipeById.name}</h3>
     <div className="likesDiv">
-      <button><h1 className="likes"><FontAwesomeIcon icon={faThumbsUp} /></h1>{recipeById.likes}</button>
+      <button onClick={handleLikes}><h1 className="likes"><FontAwesomeIcon icon={faThumbsUp} /></h1>{recipeById.likes}</button>
     </div>
     <div className="favoriteDiv">
       <button onClick={addToProfile}>
