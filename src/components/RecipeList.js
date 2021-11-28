333bbed1638f0e38f231cc62e805d5ddb22ed523
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-28 12:50:54 -0500

    new stratedy for format

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -20,43 +20,23 @@ export default function RecipeList(props) {
       <h2>Search Results</h2>
       
       {recipes.map((recipe, key) => (
-       <div class="card mb-3" style="max-width: 540px;"> 
-       <div class="row no-gutters">
-         <div class="col-md-4">
-           <img src={recipe.image} class="card-img" alt="..." height="70" width="100"/>
-         </div>
-         <div class="col-md-8">
-           <div class="card-body">
-             <h5 class="card-title">Card title</h5>
-             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-           </div>
-         </div>
-       </div>
-     </div>
+        <div className="searchItem">
+    <Container>
+        <article key={key} onClick={handleClick} id={recipe.id}>
+        <Col>
+        <img id={recipe.id} src={recipe.image} alt="display image" height="140" width="200" />
+        </Col>
+        <Col>
+          <h3 id={recipe.id}>{recipe.name}</h3>
+          <p id={recipe.id}>Category: {recipe.category_name}</p>
+          <p id={recipe.id}>Likes:{recipe.likes}</p>
+        </Col>
+        </article>
+        </Container>
+        </div>
       ))}
        
     </div>
   </>);
 }
 
-// {recipes.map((recipe, key) => (
-//   <div className="searchItem">
-// <Container>
-//   <article key={key} onClick={handleClick} id={recipe.id}>
-//   <Col>
-//   <img id={recipe.id} src={recipe.image} alt="display image" height="70" width="100" />
-//   </Col>
-//   <Col>
-//     <h3 id={recipe.id}>{recipe.name}</h3>
-//     <p id={recipe.id}>Category: {recipe.category_name}</p>
-//     <p id={recipe.id}>Likes:{recipe.likes}</p>
-//   </Col>
-//   </article>
-//   </Container>
-//   </div>
-// ))}
- 
-// </div>
-// </>);
-// }
