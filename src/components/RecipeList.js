ada180f1463bd535d2552a585ec51f647879632e
hlai16 <gloriajoanlai@gmail.com>
2021-11-29 16:04:01 -0500

    search results styling done

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,6 @@
 import { useLocation } from "react-router";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
-import { Col,Container,Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import './RecipeList.scss';
@@ -13,40 +12,41 @@ export default function RecipeList(props) {
 
   let navigate = useNavigate()
   const handleClick = function (event) {
-    console.log("recipe",event.target.id)
+    console.log("recipe", event.target.id)
     navigate(`/SingleRecipe`, { state: Number(event.target.id) })
   }
 
   return (< >
     <NavBar setToken={props.setToken} />
     <div className="recipeListDiv">
-    <Container>
-      <div> 
-      <h2>Search Results</h2>
-      </div>
-      {recipes.map((recipe, key) => (
-        <div id={recipe.id} className="searchItem">
-        <article key={key} onClick={handleClick} id={recipe.id}> 
-        <Container id={recipe.id}>
-  
-        <Row id={recipe.id}> <h3 id={recipe.id}>{recipe.name}</h3> </Row>
- 
-        <Col id={recipe.id}>
-        <div className="Search--img">
-        <img id={recipe.id} src={recipe.image} alt="display image" height="70" width="100" />
-  
+      <section>
+        <div>
+          <h1>Search Results</h1>
         </div>
-        </Col>
-        <Col id={recipe.id}>
-          <p id={recipe.id}>Category: {recipe.category_name}</p>
-          <p id={recipe.id} ><FontAwesomeIcon icon={faThumbsUp} /> {recipe.likes} Likes </p>
-        </Col>
-        
-        </Container>
-        </article>
-        </div>
-      ))}
-     </Container>
+        {recipes.map((recipe, key) => (
+          <div id={recipe.id} className="searchItem">
+            <article key={key} onClick={handleClick} id={recipe.id}>
+              <div id={recipe.id}>
+
+                <h3 id={recipe.id}>{recipe.name}</h3> 
+
+                <section id={recipe.id}>
+                  <div className="Search--img">
+                    <img id={recipe.id} src={recipe.image} alt="display image" height="70" width="100" />
+
+                  </div>
+                </section>
+                <section id={recipe.id}>
+                  <p id={recipe.id}>Category: {recipe.category_name}</p>
+                  <p id={recipe.id} ><FontAwesomeIcon icon={faThumbsUp} /> {recipe.likes} Likes </p>
+                </section>
+
+              </div>
+            </article>
+          </div>
+        ))}
+      </section>
+      {/* </Container> */}
     </div>
   </>);
 }
